dacd1b0db7bd13e8b26350f50e187d9f3f2bcf27
ThanhTam27 <thanhtam180298@gmail.com>
2020-03-15 15:34:37 +0700

    Adding and test success redux presist

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Form from '../../components/Form';
 
-const Home = () =>{
-  return(
+const Home = () => {
+  return (
     <div>
-      <div style={{textAlign: "center"}}>Hello! this is React with Parcel</div>
+      <div style={{ textAlign: "center" }}>
+        Hello! this is React with Parcel
+      </div>
+      <Form />
     </div>
   );
 }
